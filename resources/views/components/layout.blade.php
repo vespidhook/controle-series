fb56e8d0886ca05d334e0714139bdafee12dffbd
Bruno Alves <vespidhook@gmail.com>
2023-06-10 11:02:45 -0300

    validando formularios usando sessoes e definindo relacionamentos

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,16 @@
     <div class="container">
         <h1>{{ $title }}</h1>
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $erro)
+                        <li>{{ $erro }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         {{ $slot }}
     </div>
 </body>
