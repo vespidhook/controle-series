320a8777b43c86a536a3e782e148e8fb6092b016
Bruno Alves <vespidhook@gmail.com>
2023-06-20 12:01:26 -0300

    listando episodios

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\SeasonsController;
+use App\Http\Controllers\EpisodesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,6 @@ Route::resource('series', SeriesController::class)
     ->except('show');
 
 Route::get('series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
